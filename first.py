3f0df927732d16031f86670071e919aba6e31303
Carolina <carolina@carolina-Ferrari-One-200.(none)>
2014-04-01 19:36:39 +0200

    My first commit

---
+++ first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding:  utf8 -*-
+"""This is a documentation string
+uytoiuytoiut"""
+
+import sys
+
+
+print (sys.path)
+print(__file__)
+
+def my_func (a):
+    result = a + 2
+    return result
+    
+b = my_func (5)
+
+print (b)
+
+
+
